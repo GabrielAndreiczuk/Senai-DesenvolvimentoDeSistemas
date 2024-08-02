a7f5526ad557a4d10cbe21bff88c1428940e19dc
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-08-02 19:57:06 -0300

    Add files via upload

---
+++ Exercícios-Aula-03/ExercicioResolvido_08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //DEFINIÇÃO DE CONSTANTE PARA CÂMBIO
+        const double taxaCambio = 5.62;
+        //ENTRADA DE DADOS
+        Console.WriteLine("CONVERSÃO DE MOEDA");
+        Console.Write("\nDigite um valor em reais: R$");
+        double reais = double.Parse(Console.ReadLine());
+        //CONVERSÃO DE VALORES
+        double dolar = reais / taxaCambio;
+        //SAÍDA DE DADOS
+        Console.WriteLine($"{reais:C} equivalem à US$ {dolar:F2}");
+        //FINALIZAÇÃO DO PROGRAMA
+        Console.Write("\nPressione qualquer tecla para continuar. . .");
+        Console.ReadKey();
+    }
+}
