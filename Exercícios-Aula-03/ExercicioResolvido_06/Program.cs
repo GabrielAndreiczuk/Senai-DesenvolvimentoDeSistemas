1d890c61be56cdc6cb971cc9989cb2bde0be58f3
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-08-02 19:56:23 -0300

    Add files via upload

---
+++ Exercícios-Aula-03/ExercicioResolvido_06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Entre com a primeira nota: ");
+        double nota1 = double.Parse(Console.ReadLine());
+        Console.Write("Entre com a segunda nota: ");
+        double nota2 = double.Parse(Console.ReadLine());
+        Console.Write("Entre com a terceira nota: ");
+        double nota3 = double.Parse(Console.ReadLine());
+
+        double media = (nota1 + nota2 + nota3) / 3;
+        Console.WriteLine($"\nSua média final é de: {media:F1}");
+
+        Console.Write("\nPressione qualquer tecla para continuar. . .");
+        Console.ReadKey();
+    }
+}
---
+++ Exercícios-Aula-03/ExercicioResolvido_07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("CADASTRO DE PRODUTO");
+        Console.Write("Informe o nome do produto: ");
+        string nome = Console.ReadLine();
+        Console.Write($"Informe o preço de {nome}: R$");
+        double preco = double .Parse(Console.ReadLine());
+        Console.Write($"Informe a quantidade de {nome}: ");
+        int quantidade = int.Parse(Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("PRODUTO CADASTRADO COM SUCESSO!");
+        Console.WriteLine($"Produto: {nome}");
+        Console.WriteLine($"Preço: {preco:C}");
+        Console.WriteLine($"Quantidade: {quantidade}");
+
+        Console.Write("\nPressione qualquer tecla para continuar. . .");
+        Console.ReadKey();
+    }
+}
