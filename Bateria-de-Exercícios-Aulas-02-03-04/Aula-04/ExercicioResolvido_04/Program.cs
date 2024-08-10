b69f4aefd94608f7e7700b1c15bb05c485795663
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-08-09 21:01:36 -0300

    Add files via upload

---
+++ Bateria-de-Exercícios-Aulas-02-03-04/Aula-04/ExercicioResolvido_04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 1, 2, 3, 4 };
+        double media = calcularMedia(numeros);
+        string nums = string.Join(", ", numeros);
+        Console.WriteLine($"A média de [{nums}] é igual a: " + media);
+    }
+    static double calcularMedia(int[] numeros)
+    {
+        int soma = 0;
+        foreach (int i in numeros)
+        {
+            soma += i;
+        }
+
+        double media = (double) soma / numeros.Length;
+        return media;
+    }
+}
---
+++ Bateria-de-Exercícios-Aulas-02-03-04/Aula-04/ExercicioResolvido_05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma frase: ");
+        string frase = Console.ReadLine();
+        InverterString inverter = new InverterString();
+        Console.WriteLine("\nA frase invertida é: " + inverter.Inverter(frase));
+    }
+}
+class InverterString
+{
+    public string Inverter(string frase)
+    { 
+        char[] charFrase = frase.ToCharArray();
+        Array.Reverse(charFrase);
+        string fraseReversa = new string (charFrase);
+        return fraseReversa;
+    }
+}
