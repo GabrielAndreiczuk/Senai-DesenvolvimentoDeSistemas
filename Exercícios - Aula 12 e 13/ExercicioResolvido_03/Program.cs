010be24d94f4b437d9d66477d9be17a22c5d21bf
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-08-14 21:54:02 -0300

    Add files via upload

---
+++ Exercícios - Aula 12 e 13/ExercicioResolvido_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public interface IPessoa
+{
+    void Falar();
+}
+public class Professor : IPessoa
+{
+    public void Falar()
+    {
+        Console.WriteLine("Boa noite, Turma!");
+    }
+}
+public class Aluno : IPessoa 
+{ 
+    public void Falar()
+    {
+        Console.WriteLine("Boa noite, Professor!");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        IPessoa professor = new Professor();
+        IPessoa aluno = new Aluno();
+
+        professor.Falar();
+        Console.WriteLine();
+        aluno.Falar();
+    }
+}
