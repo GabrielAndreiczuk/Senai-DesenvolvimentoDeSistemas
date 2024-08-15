a5249ceb514945f7b3d3bc5d823dae243072694e
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-08-14 21:52:59 -0300

    Add files via upload

---
+++ Exercícios - Aula 12 e 13/ExercicioResolvido_01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+// DEFINE A INTERFACE. A INTERFACE DEFINE OS MÉTODOS QUE UMA CLASSE DEVE IMPLEMENTAR
+public interface IVeiculo
+{
+    void Ligar();
+    void Desligar();
+}
+
+// CRIAÇÃO DA CLASSE CARRO, COM METODOS DEFINIDOS PELA INTERFACE VEÍCULO
+public class Carro : IVeiculo
+{
+    public void Ligar()
+    {
+        Console.WriteLine("O carro foi ligado.");
+    }
+
+    public void Desligar()
+    {
+        Console.WriteLine("O carro foi desligado.");
+    }
+}
+
+// CRIAÇÃO DA CLASSE MOTO, COM METODOS DEFINIDOS PELA INTERFACE VEÍCULO
+public class Moto : IVeiculo
+{
+    public void Ligar()
+    {
+        Console.WriteLine("A moto foi ligada.");
+    }
+
+    public void Desligar()
+    {
+        Console.WriteLine("A moto foi desligada.");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        IVeiculo meuCarro = new Carro();
+        IVeiculo minhaMoto = new Moto();
+
+        meuCarro.Ligar();
+        meuCarro.Desligar();
+        Console.WriteLine();
+
+        minhaMoto.Ligar();
+        minhaMoto.Desligar();
+    }
+}
---
+++ Exercícios - Aula 12 e 13/ExercicioResolvido_02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public abstract class Forma
+{
+    public abstract double CalcularArea();
+}
+
+//CLASSE RETÂNGULO QUE HERDA DA CLASSE ABSTRATA FORMA
+public class Retangulo : Forma
+{
+    public double Largura { get; set; } //PRORPIEDADES AUTO-IMPLEMENTADAS, SEM NECESSIDADE DE VALIDAÇÃO
+    public double Altura { get; set; }
+
+    public override double CalcularArea()
+    {
+        return Largura * Altura;
+    }
+}
+
+//CLASSE CIRCULO QUE HERDA DA CLASSE ABSTRATA FORMA
+public class Circulo : Forma
+{
+    public double Raio { get; set; }//PRORPIEDADE AUTO-IMPLEMENTADA, SEM NECESSIDADE DE VALIDAÇÃO
+
+    public override double CalcularArea()
+    {
+        return Math.PI * Math.Pow(Raio, 2);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        //INSTÂNCIANDO RETANGULO E PASSANDO VALOR DE PARÂMETROS
+        Retangulo retangulo = new Retangulo
+        {
+            Largura = 5.0,
+            Altura = 10.0
+        };
+
+        //INSTÂNCIANDO CIRCULO E PASSANDO VALOR DE PARÂMETROS
+        Circulo circulo = new Circulo
+        {
+            Raio = 10.0
+        };
+
+        //MOSTRANDO RESULTADOS NA TELA
+        Console.WriteLine($"Área do Retângulo: {retangulo.CalcularArea()}");
+        Console.WriteLine($"Área do Círculo: {circulo.CalcularArea():F2}");
+    }
+}
