e8cd0700b72999f11cda647a38479d7a6566da02
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-08-19 19:24:12 -0300

    Add files via upload

---
+++ Exercícios - Aula 16 e 17/ExercicioResolvido_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[5];
+        
+        for(int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write("Informe um número inteiro: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("\nArray invertido: ");
+        for(int i = numeros.Length -1; i >= 0; i--)
+        {
+            Console.WriteLine(numeros[i]);
+        }
+    }
+}
