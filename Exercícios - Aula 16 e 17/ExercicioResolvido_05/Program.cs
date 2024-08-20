0f5fe009671949295cdf2ad6726b51a4abf2fd07
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-08-19 21:17:51 -0300

    Add files via upload

---
+++ Exercícios - Aula 16 e 17/ExercicioResolvido_05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> dicionario = new Dictionary<int, string>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Informe uma palavra: ");
+            dicionario.Add(i+1, Console.ReadLine());
+        }
+        Console.WriteLine("\nPares com chaves e valores: ");
+        foreach(KeyValuePair<int, string> par in dicionario)
+        {
+            Console.WriteLine($"Chave: {par.Key} Valor: {par.Value}");
+        }
+    }
+}
