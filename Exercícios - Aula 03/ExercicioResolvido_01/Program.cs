182d81c0981410d126ac53332ed6b3c5dc9eee62
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-08-26 20:57:19 -0300

    Add files via upload

---
+++ Exercícios - Aula 03/ExercicioResolvido_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+ class Program()
+{
+    static void Main()
+    {
+        Console.Write("Prezado usuário(a), digite seu nome: ");
+        string nome = Console.ReadLine();
+        Console.Write("Informe sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nObrigado {nome}, você tem {idade} anos!\n");
+
+        Console.Write("Pressione qualquer tecla para continuar. . .");
+        Console.ReadKey();
+    }
+}
