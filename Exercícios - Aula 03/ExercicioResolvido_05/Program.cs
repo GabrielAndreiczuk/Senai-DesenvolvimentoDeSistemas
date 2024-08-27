89f4278ddca40a323283b30b1f924a60ac0ef67e
Gabriel Andreiczuk <88723696+GabrielAndreiczuk@users.noreply.github.com>
2024-08-26 21:02:50 -0300

    Add files via upload

---
+++ Exercícios - Aula 03/ExercicioResolvido_05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int num = (int) double.Parse(Console.ReadLine());
+        
+        if(num % 2  == 0)
+        {
+            Console.WriteLine($"{num} é par!");
+        }
+        else
+        {
+            Console.WriteLine($"{num} é impar!");
+        }
+        Console.Write("\n\nPressione qualquer tecla para continuar. . .");
+        Console.ReadKey();
+    }
+}
